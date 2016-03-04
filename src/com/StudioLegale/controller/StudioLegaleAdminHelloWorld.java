ccdbc876395fa0221cbc489a7bd75c58fa3a3d48
matrixxx <espfranc@gmail.com>
2016-03-04 14:17:33 +0100

    change xml name

--- src/com/StudioLegale/controller/StudioLegaleAdminHelloWorld.java
+++ src/com/StudioLegale/controller/StudioLegaleAdminHelloWorld.java
@@ -1,4 +1,4 @@
-package com.StudioLegaleAdmin.controller;
+package com.StudioLegale.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
